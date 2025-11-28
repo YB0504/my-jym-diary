db4a631f276e7d34f31c3e4900c68d4f09c8f8fc
YB0504 <yeo9411@naver.com>
2025-11-28 23:02:24 +0900

    first project commit

---
+++ src/main/java/com/myjymdiary/myjym/MyjymApplication.java
@@ -0,0 +1,13 @@
+package com.myjymdiary.myjym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myjymdiary/myjym/MyjymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjymdiary.myjym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
